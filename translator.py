0fb10d2a1239af0dba6789a9e365f2bd9cac0ba7
Aksahy Chopade <63453600+AkshayChopade07@users.noreply.github.com>
2020-09-21 20:02:45 +0530

    Language  Translator Using Python
    The Code For Language  Translation Using Python

---
+++ translator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 20:39:46 2020
+
+@author: Immortal
+"""
+
+
+ from translate import Translator
+       translator = Translator (from_lang = 'English',to_lang = 'German')
+  result = translator.translate ('I Love You')
+  print (result)
