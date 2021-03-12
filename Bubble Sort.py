d7284f2373e95d08e800a3fc9bd524fd3247b682
Aksahy Chopade <63453600+AkshayChopade07@users.noreply.github.com>
2021-03-12 23:34:30 +0530

    Bubble Sort Algorithm
    Bubble Sort is a sorting algorithm used to sort list items in ascending order by comparing two adjacent values. If the first value is higher than the second value, the first value takes the second value position, while the second value takes the first value position. If the first value is lower than the second value, then no swapping is done.

---
+++ Bubble Sort.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 12 23:31:26 2021
+
+@author: Immortal
+"""
+
+def bubbleSort( theSeq ):
+    n = len( theSeq )
+
+    for i in range( n - 1 ) :
+        flag = 0
+
+        for j in range(n - 1) :
+            
+            if theSeq[j] > theSeq[j + 1] : 
+                tmp = theSeq[j]
+                theSeq[j] = theSeq[j + 1]
+                theSeq[j + 1] = tmp
+                flag = 1
+
+        if flag == 0:
+            break
+
+    return theSeq
+
+el = [21,6,9,33,3] 
+
+result = bubbleSort(el)
+
+print (result)
